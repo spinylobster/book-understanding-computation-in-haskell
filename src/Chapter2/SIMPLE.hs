9bc06629af2df8edcce6b43fa845491b144df073
spinylobster <spinylobster@outlook.com>
2019-02-17 23:37:27 +0900

    from Chapter 2 to 2.5.3 (2.6 is remained)

---
+++ src/Chapter2/SIMPLE.hs
@@ -0,0 +1,154 @@
+{-# LANGUAGE ViewPatterns #-}
+{-# LANGUAGE TupleSections #-}
+
+module Chapter2.SIMPLE
+  ( SIMPLE(..)
+  , Expression(..)
+  , Statement(..)
+  , Env
+  , inspect
+  , run
+  ) where
+
+import Data.Char
+import qualified Data.Map.Strict as Map
+
+type Env = Map.Map String Expression
+type Machine a = (a, Env)
+
+class Show a => SIMPLE a where
+  reducible :: a -> Bool
+  reduce :: Machine a -> Machine a
+  evaluate :: Machine a -> Machine a
+  toRuby :: a -> String
+
+run :: SIMPLE a => Machine a -> IO ()
+run (program, env) = do
+  if reducible program then do
+    print program
+    run $ reduce (program, env)
+  else print program
+
+data Expression =
+  Variable String
+  | Number Int
+  | Add Expression Expression
+  | Multiply Expression Expression
+  | Boolean Bool
+  | And Expression Expression
+  | Or Expression Expression
+  | Not Expression
+  | LessThan Expression Expression
+  deriving (Eq)
+
+instance Show Expression where
+  show (Variable name) = name
+  show (Number a) = show a
+  show (Add left right) = show left ++ " + " ++ show right
+  show (Multiply left right) = show left ++ " * " ++ show right
+  show (Boolean b) = show b
+  show (And left right) = show left ++ " && " ++ show right
+  show (Or left right) = show left ++ " || " ++ show right
+  show (Not b) = "!" ++ show b
+  show (LessThan left right) = show left ++ " < " ++ show right
+
+instance SIMPLE Expression where
+  reducible (Number _) = False
+  reducible (Boolean _) = False
+  reducible _ = True
+
+  reduce (exp, env) = (reduce' exp, env)
+    where
+      reduce' (Variable name) = env Map.! name
+      reduce' (Add (Number left) (Number right)) = Number $ left + right
+      reduce' (Add left right) = Add left' right'
+        where (left', right') = reduceFromLeft env (left, right)
+      reduce' (Multiply (Number left) (Number right)) = Number $ left * right
+      reduce' (Multiply left right) = Multiply left' right'
+        where (left', right') = reduceFromLeft env (left, right)
+      reduce' (LessThan (Number left) (Number right)) = Boolean $ left < right
+      reduce' (LessThan left right) = LessThan left' right'
+        where (left', right') = reduceFromLeft env (left, right)
+
+  evaluate (exp, env) = (evaluate' exp, env)
+    where
+      evalF = fst . evaluate . (, env)
+      evaluate' (Variable name) = env Map.! name
+      evaluate' self@(Number _) = self
+      evaluate' (Add left right) = Number $ left' + right'
+        where [Number left', Number right'] = evalF <$> [left, right]
+      evaluate' (Multiply left right) = Number $ left' * right'
+        where [Number left', Number right'] = evalF <$> [left, right]
+      evaluate' self@(Boolean _) = self
+      evaluate' (LessThan left right) = Boolean $ left' < right'
+        where [Number left', Number right'] = evalF <$> [left, right]
+
+  toRuby (Number value) = "-> e { " <> show value <> " }"
+  toRuby (Boolean value) = "-> e { " <> (toLower <$> show value) <> " }"
+  toRuby (Variable name) = "-> e { e[:" <> name <> "] }"
+  toRuby (Add left right) = "-> e { (" <> toRuby left <> ").call(e) + (" <> toRuby right <> ").call(e) }"
+  toRuby (Multiply left right) = "-> e { (" <> toRuby left <> ").call(e) * (" <> toRuby right <> ").call(e) }"
+  toRuby (LessThan left right) = "-> e { (" <> toRuby left <> ").call(e) < (" <> toRuby right <> ").call(e) }"
+
+data Statement =
+    DoNothing
+  | Assign String Expression
+  | If Expression Statement Statement
+  | Sequence Statement Statement
+  | While Expression Statement
+  deriving (Eq)
+
+instance Show Statement where
+  show DoNothing = "do-nothing"
+  show (Assign name exp) = name ++ " = " ++ show exp
+  show (If cond cons alt) = "if (" ++ show cond ++ ") { " ++ show cons ++ " } else { " ++ show alt ++ " }"
+  show (Sequence first second) = show first ++ "; " ++ show second
+  show (While cond body) = "while (" ++ show cond ++ ") { " ++ show body ++ " }"
+
+instance SIMPLE Statement where
+  reducible (DoNothing) = False
+  reducible _ = True
+
+  reduce ((Assign name exp@(reducible -> False)), env) = (DoNothing, env')
+    where env' = Map.insert name exp env
+  reduce ((Assign name exp@(reducible -> True)), env) = (Assign name exp', env)
+    where exp' = fst . reduce $ (exp, env)
+  reduce (If (Boolean True) cons _, env) = (cons, env)
+  reduce (If (Boolean False) _ alt, env) = (alt, env)
+  reduce (If cond@(reducible -> True) cons alt, env) = (stat', env)
+    where stat' = If (fst . reduce $ (cond, env)) cons alt
+  reduce (Sequence DoNothing second, env) = (second, env)
+  reduce (Sequence first second, env) = (Sequence first' second, env')
+    where (first', env') = reduce (first, env)
+  reduce (stat@(While cond body), env) = (stat', env)
+    where stat' = If cond (Sequence body stat) DoNothing
+
+  evaluate self@(DoNothing, env) = self
+  evaluate ((Assign name exp), env) = (DoNothing, env')
+    where env' = Map.insert name value env
+          value = fst . evaluate $ (exp, env)
+  evaluate (If cond cons alt, env) = if cond' then cons' else alt'
+    where Boolean cond' = fst . evaluate $ (cond, env)
+          cons' = evaluate (cons, env)
+          alt' = evaluate (alt, env)
+  evaluate (Sequence first second, env) = evaluate (second, env')
+    where env' = snd . evaluate $ (first, env)
+  evaluate (stat@(While cond body), env) = evaluate (stat', env')
+    where Boolean cond' = fst . evaluate $ (cond, env)
+          stat' = if cond' then stat else DoNothing
+          env' = if cond' then snd . evaluate $ (body, env) else env
+
+  toRuby DoNothing = "-> e { e }"
+  toRuby (Assign name value) = "-> e { e.merge({ :" <> name <> " => (" <> toRuby value <> ").call(e) }) }"
+  toRuby (If cond cons alt) = "-> e { if (" <> toRuby cond <> ").call(e) then " <> toRuby cons <> " else " <> toRuby alt <> " end }"
+  toRuby (Sequence first second) = "-> e { " <> toRuby second <> ".call(" <> toRuby first <> ".call(e)) }"
+  toRuby (While cond body) = "-> e { while (" <> toRuby cond <> ").call(e); e = (" <> toRuby body <> ").call(e); end; e }"
+
+inspect :: SIMPLE a => a -> String
+inspect exp = "<<" <> show exp <> ">>"
+
+reduceFromLeft :: SIMPLE a => Env -> (a, a) -> (a, a)
+reduceFromLeft env (left@(reducible -> True), right) = (left', right)
+  where (left', env') = reduce (left, env)
+reduceFromLeft env (left, right@(reducible -> True)) = (left, right')
+  where (right', env') = reduce (right, env)
---
+++ test/Chapter2/SIMPLESpec.hs
@@ -0,0 +1,322 @@
+module Chapter2.SIMPLESpec (spec) where
+
+import Test.Hspec
+import Chapter2.SIMPLE
+import qualified Data.Map.Strict as Map
+
+spec :: Spec
+spec = do
+  let reduceN n = foldl (.) reduce $ replicate (n-1) reduce
+
+  describe "SIMPLE.Expression" $ do
+    let env = Map.empty :: Env
+
+    it "should be an instance of Show" $ do
+      let exp = Add
+            (Multiply (Number 1) (Number 2))
+            (Multiply (Number 3) (Number 4))
+      inspect exp `shouldBe` "<<1 * 2 + 3 * 4>>"
+      let exp = Number 5
+      inspect exp `shouldBe` "<<5>>"
+
+    describe "reduce" $ do
+      context ("with env: " <> show env) $ do
+        let exp = Add
+              (Multiply (Number 1) (Number 2))
+              (Multiply (Number 3) (Number 4))
+        describe (inspect exp) $ do
+          let machine = (exp, env)
+
+          let expected = Add (Number 2) (Multiply (Number 3) (Number 4))
+          it ("reduce once should be: " <> show expected) $ do
+            let actual = fst . reduce $ machine
+            actual `shouldBe` expected
+
+          let expected = Add (Number 2) (Number 12)
+          it ("reduce twice should be: " <> show expected) $ do
+            let actual = fst . (reduceN 2) $ machine
+            actual `shouldBe` expected
+
+          let expected = Number 14
+          it ("reduce 3 times should be: " <> show expected) $ do
+            let actual = fst . (reduceN 3) $ machine
+            actual `shouldBe` expected
+  
+        let exp = LessThan (Number 5) (Add (Number 2) (Number 2))
+        describe (inspect exp) $ do
+          let machine = (exp, env)
+
+          let expected = LessThan (Number 5) (Number 4)
+          it ("reduce once should be: " <> show expected) $ do
+            let actual = fst . reduce $ machine
+            actual `shouldBe` expected
+
+          let expected = Boolean False
+          it ("reduce twice should be: " <> show expected) $ do
+            let actual = fst . (reduceN 2) $ machine
+            actual `shouldBe` expected
+
+      let env = Map.fromList [("x", Number 3), ("y", Number 4)]
+      context ("with env: " <> show env) $ do
+        let exp = Add (Variable "x") (Variable "y")
+        let machine = (exp, env)
+
+        describe (inspect exp) $ do
+          let expected = Add (Number 3) (Variable "y")
+          it ("reduce once should be: " <> show expected) $ do
+            let actual = fst . reduce $ machine
+            actual `shouldBe` expected
+
+          let expected = Add (Number 3) (Number 4)
+          it ("reduce twice should be: " <> show expected) $ do
+            let actual = fst . (reduceN 2) $ machine
+            actual `shouldBe` expected
+
+          let expected = Number 7
+          it ("reduce 3 times should be: " <> show expected) $ do
+            let actual = fst . (reduceN 3) $ machine
+            actual `shouldBe` expected
+
+    describe "evaluate" $ do
+      let env = Map.empty :: Env
+      context ("with env: " <> show env) $ do
+        let exp = Number 23
+        describe (inspect exp) $ do
+          let machine = (exp, env)
+
+          let expected = Number 23
+          it ("evaluate should be: " <> show expected) $ do
+            let actual = fst . evaluate $ machine
+            actual `shouldBe` expected
+
+      let env = Map.singleton "x" $ Number 23
+      context ("with env: " <> show env) $ do
+        let exp = Variable "x"
+        describe (inspect exp) $ do
+          let machine = (exp, env)
+
+          let expected = Number 23
+          it ("evaluate should be: " <> show expected) $ do
+            let actual = fst . evaluate $ machine
+            actual `shouldBe` expected
+
+      let env = Map.fromList [("x", Number 2), ("y", Number 5)]
+      context ("with env: " <> show env) $ do
+        let exp = LessThan (Add (Variable "x") (Number 2)) (Variable "y")
+        describe (inspect exp) $ do
+          let machine = (exp, env)
+
+          let expected = Boolean True
+          it ("evaluate should be: " <> show expected) $ do
+            let actual = fst . evaluate $ machine
+            actual `shouldBe` expected
+
+    describe "toRuby" $ do
+      let exp = Number 5
+      describe (inspect exp) $ do
+        let expected = "-> e { 5 }"
+        it ("should be: " <> expected) $ do
+          let actual = toRuby exp
+          actual `shouldBe` expected
+
+      let exp = Boolean False
+      describe (inspect exp) $ do
+        let expected = "-> e { false }"
+        it ("should be: " <> expected) $ do
+          let actual = toRuby exp
+          actual `shouldBe` expected
+
+      let exp = Variable "x"
+      describe (inspect exp) $ do
+        let expected = "-> e { e[:x] }"
+        it ("should be: " <> expected) $ do
+          let actual = toRuby exp
+          actual `shouldBe` expected
+
+      let exp = Add (Variable "x") (Number 1)
+      describe (inspect exp) $ do
+        let expected = "-> e { (-> e { e[:x] }).call(e) + (-> e { 1 }).call(e) }"
+        it ("should be: " <> expected) $ do
+          let actual = toRuby exp
+          actual `shouldBe` expected
+
+      let exp = LessThan (Add (Variable "x") (Number 1)) (Number 3)
+      describe (inspect exp) $ do
+        let expected = "-> e { (-> e { (-> e { e[:x] }).call(e) + (-> e { 1 }).call(e) }).call(e) < (-> e { 3 }).call(e) }"
+
+        it ("should be: " <> expected) $ do
+          let actual = toRuby exp
+          actual `shouldBe` expected
+          
+  describe "SIMPLE.Statement" $ do
+    let emptyEnv = Map.empty
+    let stat1 = Assign "x" $ Add (Variable "x") (Number 1)
+
+    it "should be an instance of Show" $ do
+      inspect stat1 `shouldBe` "<<x = x + 1>>"
+
+    describe "reduce" $ do
+      let env = Map.singleton "x" $ Number 2
+      context ("with env: " <> show env) $ do
+        describe (inspect stat1) $ do
+          let machine = (stat1, env)
+
+          let expected = (Assign "x" $ Add (Number 2) (Number 1), env)
+          it ("reduce once should be: " <> show expected) $ do
+            let actual = reduce machine
+            actual `shouldBe` expected
+
+          let expected = (Assign "x" $ Number 3, env)
+          it ("reduce twice should be: " <> show expected) $ do
+            let actual = reduceN 2 machine
+            actual `shouldBe` expected
+
+          let env' = Map.singleton "x" $ Number 3
+          let expected = (DoNothing, env')
+          it ("reduce 3 times should be: " <> show expected) $ do
+            let actual = reduceN 3 machine
+            actual `shouldBe` expected
+  
+      let env = Map.singleton "x" $ Boolean True
+      let stat = If (Variable "x") (Assign "y" (Number 1)) (Assign "y" (Number 2))
+      context ("with env: " <> show env) $ do
+        describe (inspect stat) $ do
+          let machine = (stat, env)
+
+          let expected = (If (Boolean True) (Assign "y" (Number 1)) (Assign "y" (Number 2)), env)
+          it ("reduce once should be: " <> show expected) $ do
+            let actual = reduce machine
+            actual `shouldBe` expected
+
+          let expected = (Assign "y" (Number 1), env)
+          it ("reduce twice should be: " <> show expected) $ do
+            let actual = reduceN 2 machine
+            actual `shouldBe` expected
+
+          let env' = Map.fromList [("x", Boolean True), ("y", Number 1)]
+          let expected = (DoNothing, env')
+          it ("reduce 3 times should be: " <> show expected) $ do
+            let actual = reduceN 3 machine
+            actual `shouldBe` expected
+  
+      let env = Map.empty :: Env
+      let onePlusOne = Add (Number 1) (Number 1)
+      let xPlusThree = Add (Variable "x") (Number 3)
+      let stat = Sequence (Assign "x" onePlusOne) (Assign "y" xPlusThree)
+      context ("with env: " <> show env) $ do
+        describe (inspect stat) $ do
+          let machine = (stat, env)
+
+          let expected = (Sequence (Assign "x" $ Number 2) (Assign "y" xPlusThree), env)
+          it ("reduce once should be: " <> show expected) $ do
+            let actual = reduce machine
+            actual `shouldBe` expected
+
+          let env' = Map.singleton "x" $ Number 2
+          let expected = (Sequence DoNothing (Assign "y" xPlusThree), env')
+          it ("reduce twice should be: " <> show expected) $ do
+            let actual = reduceN 2 machine
+            actual `shouldBe` expected
+
+          let expected = (Assign "y" xPlusThree, env')
+          it ("reduce 3 times should be: " <> show expected) $ do
+            let actual = reduceN 3 machine
+            actual `shouldBe` expected
+
+          let expected = (Assign "y" $ Add (Number 2) (Number 3), env')
+          it ("reduce 4 times should be: " <> show expected) $ do
+            let actual = reduceN 4 machine
+            actual `shouldBe` expected
+
+          let expected = (Assign "y" $ Number 5, env')
+          it ("reduce 5 times should be: " <> show expected) $ do
+            let actual = reduceN 5 machine
+            actual `shouldBe` expected
+
+          let env' = Map.fromList [("x", Number 2), ("y", Number 5)]
+          let expected = (DoNothing, env')
+          it ("reduce 6 times should be: " <> show expected) $ do
+            let actual = reduceN 6 machine
+            actual `shouldBe` expected
+
+      let env = Map.singleton "x" $ Number 1
+      let cond = LessThan (Variable "x") (Number 5)
+      let body = Assign "x" (Multiply (Variable "x") (Number 3))
+      let stat = While cond body
+      context ("with env: " <> show env) $ do
+        describe (inspect stat) $ do
+          let machine = (stat, env)
+
+          let expected = (If (Boolean True) (Sequence body stat) DoNothing, env)
+          it ("reduce 3 times should be: " <> show expected) $ do
+            let actual = reduceN 3 machine
+            actual `shouldBe` expected
+
+          let expected = (Sequence (Assign "x" (Number 3)) stat, env)
+          it ("reduce 6 times should be: " <> show expected) $ do
+            let actual = reduceN 6 machine
+            actual `shouldBe` expected
+
+          let env' = Map.singleton "x" $ Number 3
+          let expected = (stat, env')
+          it ("reduce 8 times should be: " <> show expected) $ do
+            let actual = reduceN 8 machine
+            actual `shouldBe` expected
+
+          let env' = Map.singleton "x" $ Number 9
+          let expected = (DoNothing, env')
+          it ("reduce 20 times should be: " <> show expected) $ do
+            let actual = reduceN 20 machine
+            actual `shouldBe` expected
+ 
+    describe "evaluate" $ do
+      let env = Map.empty :: Env
+      context ("with env: " <> show env) $ do
+        let onePlusOne = Add (Number 1) (Number 1)
+        let xPlusThree = Add (Variable "x") (Number 3)
+        let stat = Sequence (Assign "x" onePlusOne) (Assign "y" xPlusThree)
+        describe (inspect stat) $ do
+          let machine = (stat, env)
+
+          let expected = (DoNothing, Map.fromList [("x", Number 2), ("y", Number 5)])
+          it ("evaluate should be: " <> show expected) $ do
+            let actual = evaluate $ machine
+            actual `shouldBe` expected
+
+      let env = Map.singleton "x" $ Number 1
+      let cond = LessThan (Variable "x") (Number 5)
+      let body = Assign "x" (Multiply (Variable "x") (Number 3))
+      let stat = While cond body
+      context ("with env: " <> show env) $ do
+        describe (inspect stat) $ do
+          let machine = (stat, env)
+
+          let expected = (DoNothing, Map.singleton "x" $ Number 9)
+          it ("evaluate should be: " <> show expected) $ do
+            let actual = evaluate $ machine
+            actual `shouldBe` expected
+
+    describe "toRuby" $ do
+      let stat = Assign "y" $ Add (Variable "x") (Number 1)
+      describe (inspect stat) $ do
+        let expected = "-> e { e.merge({ :y => (-> e { (-> e { e[:x] }).call(e) + (-> e { 1 }).call(e) }).call(e) }) }"
+        it ("should be: " <> expected) $ do
+          let actual = toRuby stat
+          actual `shouldBe` expected
+
+      let stat = DoNothing
+      describe (inspect stat) $ do
+        let expected = "-> e { e }"
+        it ("should be: " <> expected) $ do
+          let actual = toRuby stat
+          actual `shouldBe` expected
+
+      let env = Map.singleton "x" $ Number 1
+      let cond = LessThan (Variable "x") (Number 5)
+      let body = Assign "x" (Multiply (Variable "x") (Number 3))
+      let stat = While cond body
+      describe (inspect stat) $ do
+        let expected = "-> e { while (-> e { (-> e { e[:x] }).call(e) < (-> e { 5 }).call(e) }).call(e); e = (-> e { e.merge({ :x => (-> e { (-> e { e[:x] }).call(e) * (-> e { 3 }).call(e) }).call(e) }) }).call(e); end; e }" 
+        it ("should be: " <> expected) $ do
+          let actual = toRuby stat
+          actual `shouldBe` expected
